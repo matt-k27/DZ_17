0e74b3a2f6dba74259da04d46218983fc704f169
Matt <matt@MacBook-Pro-Matt.local>
2024-03-15 21:26:13 +0100

    capitalize

---
+++ main.py
@@ -0,0 +1,23 @@
+print('DZ17')
+
+
+def correct_sentence(text):
+    if not text.endswith('.'):
+        text += '.'
+    return text[0].upper() + text[1:]
+
+
+assert correct_sentence("greetings, friends") == "Greetings, friends.", 'Test1'
+assert correct_sentence("hello") == "Hello.", 'Test2'
+assert correct_sentence("Greetings. Friends") == "Greetings. Friends.", 'Test3'
+assert correct_sentence("Greetings, friends.") == "Greetings, friends.", 'Test4'
+assert correct_sentence("greetings, friends.") == "Greetings, friends.", 'Test5'
+
+print('Thank you for using')
+
+###########################
+
+# def correct_sentence(text):
+#     if not text.endswith('.'):
+#         text += '.'
+#     return text.capitalize()
