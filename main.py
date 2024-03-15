411aa506d6ff0bf89acbbaa767bc8d9188ef771e
Matt <matt@MacBook-Pro-Matt.local>
2024-03-15 21:30:27 +0100

    some bugs

--- main.py
+++ main.py
@@ -14,10 +14,3 @@ assert correct_sentence("Greetings, friends.") == "Greetings, friends.", 'Test4'
 assert correct_sentence("greetings, friends.") == "Greetings, friends.", 'Test5'
 
 print('Thank you for using')
-
-###########################
-
-# def correct_sentence(text):
-#     if not text.endswith('.'):
-#         text += '.'
-#     return text.capitalize()
